7e0759ce8ce5e407ea4bbaf2fecef794f35987bd
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2024-05-11 23:20:25 +0530

    Initial commit

---
+++ Assets/Scripts/PadBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadBehaviour : MonoBehaviour {
+
+	public float jumpForce = 10f;
+
+	private void OnCollisionEnter2D(Collision2D other){
+		if(other.relativeVelocity.y <= 0){
+			if( other.collider.TryGetComponent<Rigidbody2D>(out var rb)) {
+				rb.velocity = new Vector2(0,jumpForce);
+			}
+		}
+	} 
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody2D playerRigidBody;
+    private float movement = 0f;
+    [SerializeField] private float _movementSpeed = 5f;
+
+    private void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * _movementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidBody.velocity = new Vector2(movement, playerRigidBody.velocity.y);
+    }
+}
