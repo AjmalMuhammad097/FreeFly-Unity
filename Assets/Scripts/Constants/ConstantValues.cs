150c55e2c4e9b1d7a5877800d78b16c68854bd33
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2024-05-16 21:39:29 +0530

    Added constant values script.

---
+++ Assets/Scripts/Constants/ConstantValues.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantValues
+{
+    public const string HIT_TRIGGER_ANIMATION = "Hit";
+
+    public const string LASTSCORE_PLAYERPREFS = "LastScore";
+    public const string BESTSCORE_PLAYERPREFS = "BestScore";
+    public const string TOTALSCORE_PLAYERPREFS = "TotalScore";
+
+
+}
