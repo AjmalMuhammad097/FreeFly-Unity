8afccaa95de9609c7fea87d005c2e1005096faa7
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2024-05-17 15:54:30 +0530

    Modified Asteroid Script.

--- Assets/Scripts/GameScene/Asteroid.cs
+++ Assets/Scripts/GameScene/Asteroid.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    [SerializeField]
-    private float _rotateSpeed = 20.0f;
-    [SerializeField]
-    private Animator _explosionAnimation;
+    [SerializeField] private float _rotateSpeed = 20.0f;
+    [SerializeField] private Animator _explosionAnimation;
 
-    void Update()
+    [SerializeField] private float _fallSpeed = 5f;
+
+    private void Update()
     {
-        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);  
+    //    transform.Translate(_fallSpeed * Time.deltaTime * Vector3.down); I can use rigid body gravity
+        transform.Rotate(_rotateSpeed * Time.deltaTime * Vector3.forward);
+
+
+/*        // Check if the asteroid is below the screen                          //Manage this like platform pooling
+        if (transform.position.y < Camera.main.transform.position.y - Camera.main.orthographicSize)
+        {
+            // Destroy the asteroid if it's below the screen
+            Destroy(gameObject);
+        }*/
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)         //Modify this
     {
         if (other.tag == "Laser")
         {
