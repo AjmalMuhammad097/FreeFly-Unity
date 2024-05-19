288e328145832d48ae1c44fbbf553f63fd2da059
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2024-05-19 22:40:25 +0530

    Added Asteroid moving functionality.

---
+++ Assets/Scripts/GameScene/AsteroidBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _fallSpeed = 5.0f; // Speed at which the asteroid falls
+    [SerializeField] private float _rotateSpeed = 20.0f;
+    [SerializeField] private Animator _explosionAnimation;
+
+    private Transform _transform;
+
+    private void Awake()
+    {
+        // Cache the transform component for better performance
+        _transform = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        // Rotate the asteroid
+        _transform.Rotate(_rotateSpeed * Time.deltaTime * Vector3.forward);
+
+        // Move the asteroid downwards
+        _transform.position += _fallSpeed * Time.deltaTime * Vector3.down;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)         //Modify this
+    {
+        if (other.tag == "Laser")
+        {
+            _explosionAnimation.SetTrigger(ConstantValues.HIT_TRIGGER_ANIMATION);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.1f);
+        }
+    }
+}
