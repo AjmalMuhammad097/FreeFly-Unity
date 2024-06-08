e6fa71235619b02366cb59314aa07ea0a9b4ca0f
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2024-06-08 21:50:53 +0530

    Implemented Settings related scripts.

---
+++ Assets/Scripts/MainMenu/SettingsPanel/SettingsPanel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPanel : MonoBehaviour
+{
+    public void SettingsPanelCloseButton()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void SettingsPanelSoundButton()
+    {
+
+    }
+
+    public void SettingsPanelMusicButton()
+    {
+
+    }
+
+    public void SettingsPanelSocialButton()
+    {
+
+    }
+
+    public void SettingsPanelCreditsButton()
+    {
+
+    }
+}
--- Assets/Scripts/MainMenu/UIManager/MainMenuUIManager.cs
+++ Assets/Scripts/MainMenu/UIManager/MainMenuUIManager.cs
@@ -3,9 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUIManager : MonoBehaviour
 {
+    [SerializeField] private SettingsPanel _settingsPanel;
 
     public void StartButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SettingsButton()
+    {
+        _settingsPanel.gameObject.SetActive(true);
     }
 }
