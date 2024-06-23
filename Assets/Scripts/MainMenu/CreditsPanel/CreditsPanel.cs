08d4e94f8fbde819b21ea00849016349d63e08b0
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2024-06-23 21:48:29 +0530

    Implemented Credits Page functionality.

---
+++ Assets/Scripts/MainMenu/CreditsPanel/CreditsPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsPanel : MonoBehaviour
+{
+    public void CreditsPanelCloseButton()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
