89d39c6b82f00aaeb82c89ded9142175b27c527b
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2024-06-23 22:42:52 +0530

    Implemented Social Panel functionality.

---
+++ Assets/Scripts/MainMenu/SocialPanel/SocialPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialPanel : MonoBehaviour
+{
+
+    //TODO need to control the text and button url from remote config
+
+    public void SocialPanelCloseButton()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void SocialPanelYoutubeButton()
+    {
+
+    }
+    public void SocialPanelInstagramButton()
+    {
+        
+    }
+    public void SocialPanelLuckButton()
+    {
+        
+    }
+
+}
