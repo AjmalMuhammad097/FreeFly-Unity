df587857a363a6b790cc3dcaab98fbce9b77c21d
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2025-06-14 13:21:32 +0530

    Update MainMenuUIManager.cs

--- Assets/Scripts/MainMenu/UIManager/MainMenuUIManager.cs
+++ Assets/Scripts/MainMenu/UIManager/MainMenuUIManager.cs
@@ -1,17 +1,32 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static Constants;
+using static Constants.AnalyticsEvents;
 
 public class MainMenuUIManager : MonoBehaviour
 {
     [SerializeField] private SettingsPanel _settingsPanel;
+    [SerializeField] private TextMeshProUGUI _privacyTermsText;
+
+    private void Start()
+    {
+        if(MyPlayerPrefs.GetBool(PlayerPrefsKeys.PRIVACY_TERMS_PLAYERPREFS, false))
+            _privacyTermsText.gameObject.SetActive(false);
+    }
 
     public void StartButton()
     {
+        MyPlayerPrefs.SetBool(PlayerPrefsKeys.PRIVACY_TERMS_PLAYERPREFS, true);
+        MyAnalytics.LogButtonEvent(ButtonName.START_MAINMENU);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void SettingsButton()
     {
+        MyAnalytics.LogButtonEvent(ButtonName.SETTINGS_MAINMENU);
         _settingsPanel.gameObject.SetActive(true);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
     }
 }
