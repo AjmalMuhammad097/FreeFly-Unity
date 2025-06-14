ab371d024904d664a8a8324dd1b5dabcfd9b3d03
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2025-06-14 13:21:19 +0530

    Update SocialPanel.cs

--- Assets/Scripts/MainMenu/SocialPanel/SocialPanel.cs
+++ Assets/Scripts/MainMenu/SocialPanel/SocialPanel.cs
@@ -1,28 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using static Constants;
+using static Constants.AnalyticsEvents;
 
 public class SocialPanel : MonoBehaviour
 {
-
-    //TODO need to control the text and button url from remote config
-
     public void SocialPanelCloseButton()
     {
+        MyAnalytics.LogButtonEvent(ButtonName.CLOSE_SOCIAL_MAINMENU);
         this.gameObject.SetActive(false);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
     }
 
     public void SocialPanelYoutubeButton()
     {
-
+        MyAnalytics.LogButtonEvent(ButtonName.LINK1_SOCIAL_MAINMENU);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
+        Application.OpenURL(GameManager.Instance.GetGameConfigData().Url.Social1);
     }
     public void SocialPanelInstagramButton()
     {
-        
+        MyAnalytics.LogButtonEvent(ButtonName.LINK2_SOCIAL_MAINMENU);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
+        Application.OpenURL(GameManager.Instance.GetGameConfigData().Url.Social2);
     }
     public void SocialPanelLuckButton()
     {
-        
+        MyAnalytics.LogButtonEvent(ButtonName.LINK3_SOCIAL_MAINMENU);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
+        Application.OpenURL(GameManager.Instance.GetGameConfigData().Url.Social3);
     }
 
 }
