9f8c9887363d176966d27608463f6fc76fcc0525
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2025-06-14 13:19:38 +0530

    Update GameOverPanel.cs

--- Assets/Scripts/GameScene/Panels/GameOver/GameOverPanel.cs
+++ Assets/Scripts/GameScene/Panels/GameOver/GameOverPanel.cs
@@ -1,33 +1,36 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static Constants;
+using static Constants.AnalyticsEvents;
 
 public class GameOverPanel : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _yourDistanceText;
     [SerializeField] private TextMeshProUGUI _bestDistanceText;
 
-    private GameManager gameManager;
-
     private void OnEnable()
     {
-        gameManager ??= GameManager.Instance;
         UpdatePanelUI();
     }
 
     private void UpdatePanelUI()
     {
-        _yourDistanceText.text = gameManager.GameData.Progress.Player.LastDistance.ToString() + Constants.ScoreData.DISTANCE_MEASURE_UNIT;
-        _bestDistanceText.text = gameManager.GameData.Progress.Player.BestDistance.ToString() + Constants.ScoreData.DISTANCE_MEASURE_UNIT;
+        _yourDistanceText.text = GameManager.Instance.GetPlayerProgress().LastDistance.ToString() + ScoreData.MEASURE_UNIT;
+        _bestDistanceText.text = GameManager.Instance.GetPlayerProgress().BestDistance.ToString() + ScoreData.MEASURE_UNIT;
     }
 
     public void RestartButton()
     {
+        MyAnalytics.LogButtonEvent(ButtonName.RESTART_GAMEOVER_GAMEMENU);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void HomeButton()
     {
+        MyAnalytics.LogButtonEvent(ButtonName.HOME_GAMEOVER_GAMEMENU);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
