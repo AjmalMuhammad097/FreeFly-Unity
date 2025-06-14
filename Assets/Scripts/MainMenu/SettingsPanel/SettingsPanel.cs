2b50aaedc84850d868db6cb7aadaef88548cf2eb
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2025-06-14 13:21:07 +0530

    Update SettingsPanel.cs

--- Assets/Scripts/MainMenu/SettingsPanel/SettingsPanel.cs
+++ Assets/Scripts/MainMenu/SettingsPanel/SettingsPanel.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static Constants;
+using static Constants.AnalyticsEvents;
 
 public class SettingsPanel : MonoBehaviour
 {
@@ -13,30 +13,51 @@ public class SettingsPanel : MonoBehaviour
     [SerializeField] private Image _musicImage;
     [SerializeField] private Sprite _musicOnSprite, _musicOffSprite;
 
+    private void OnEnable()
+    {
+        UpdateAudioIcons();
+    }
+
     public void SettingsPanelCloseButton()
     {
+        MyAnalytics.LogButtonEvent(ButtonName.CLOSE_SETTINGS_MAINMENU);
         this.gameObject.SetActive(false);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
     }
 
     public void SettingsPanelSoundButton()
     {
-        //TODO toggle Sprites
-        AudioManager.Instance.ToggleSFX();
+        MyAnalytics.LogButtonEvent(ButtonName.SOUND_SETTINGS_MAINMENU);
+        AudioManager.Instance.ToggleSound();
+        _soundImage.sprite = AudioManager.Instance.IsSFXOn ? _soundOnSprite : _soundOffSprite;
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
     }
 
     public void SettingsPanelMusicButton()
     {
-        //TODO toggle Sprites
+        MyAnalytics.LogButtonEvent(ButtonName.MUSIC_SETTINGS_MAINMENU);
         AudioManager.Instance.ToggleMusic();
+        _musicImage.sprite = AudioManager.Instance.IsMusicOn ? _musicOnSprite : _musicOffSprite;
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
     }
 
     public void SettingsPanelSocialButton()
     {
+        MyAnalytics.LogButtonEvent(ButtonName.SOCIAL_SETTINGS_MAINMENU);
         _socialPanel.gameObject.SetActive(true);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
     }
 
     public void SettingsPanelCreditsButton()
     {
+        MyAnalytics.LogButtonEvent(ButtonName.CREDITS_SETTINGS_MAINMENU);
         _creditsPanel.gameObject.SetActive(true);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
+    }
+
+    private void UpdateAudioIcons()
+    {
+        _soundImage.sprite = AudioManager.Instance.IsSFXOn ? _soundOnSprite : _soundOffSprite;
+        _musicImage.sprite = AudioManager.Instance.IsMusicOn ? _musicOnSprite : _musicOffSprite;
     }
 }
