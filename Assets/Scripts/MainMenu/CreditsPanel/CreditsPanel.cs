35b06707a3edeb59bb5a769a6dbf667425c79409
Muhammad Ajmal P M <muhammadammi313@gmail.com>
2025-06-14 13:20:27 +0530

    Update CreditsPanel.cs

--- Assets/Scripts/MainMenu/CreditsPanel/CreditsPanel.cs
+++ Assets/Scripts/MainMenu/CreditsPanel/CreditsPanel.cs
@@ -1,12 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static Constants;
+using static Constants.AnalyticsEvents;
 
 public class CreditsPanel : MonoBehaviour
 {
-    //TODO need to control the text content from Remote Config.
+    [SerializeField] private TextMeshProUGUI _creditsText;
+
+    private void Start()
+    {
+        UpdateCreditsPanelUI();
+    }
+
+    private void UpdateCreditsPanelUI()
+    {
+        if (_creditsText == null)
+            return;
+        _creditsText.text = GameManager.Instance.GetGameConfigData()?.Texts?.Credits;
+    }
+
     public void CreditsPanelCloseButton()
     {
+        MyAnalytics.LogButtonEvent(ButtonName.CLOSE_CREDITS_MAINMENU);
+        AudioManager.Instance.PlaySound(AudioConstants.SFX_POSITIVE_BUTTON_2);
         this.gameObject.SetActive(false);
     }
 }
